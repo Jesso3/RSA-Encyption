50e82e5acdb202025833797780bb05bf8ba1303a
Jesso3 <jesse2.olbrechts@gmail.com>
2024-11-10 08:46:09 +0100

    all files for rsa encryption

---
+++ rsa.c
@@ -0,0 +1,202 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_MESSAGE_SIZE 100000
+#define FIXED_WIDTH 8
+
+bool isValidPrime(long long num, long long limit) {
+  long long root = (long long)sqrtl((long double)num) + 1;
+  if (num > limit || num <= 1) {
+    return false;
+  }
+
+  for (long long i = 2; i < root; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+long long modInverse(long long e, long long phi) {
+  long long t = 0, newT = 1;
+  long long r = phi, newR = e;
+  while (newR != 0) {
+    long long quotient = r / newR;
+    t -= quotient * newT;
+    r -= quotient * newR;
+    long long tempT = t;
+    t = newT;
+    newT = tempT;
+    long long tempR = r;
+    r = newR;
+    newR = tempR;
+  }
+  if (r > 1)
+    return -1;
+  if (t < 0)
+    t += phi;
+  return t;
+}
+
+long long modXP(long long base, long long exponent, long long mod) {
+  long long result = 1;
+  base = base % mod;
+
+  while (exponent > 0) {
+    if (exponent % 2 == 1) {
+      result = (result * base) % mod;
+    }
+
+    base = ((base % mod) * (base % mod)) % mod;
+    exponent /= 2;
+  }
+  return result;
+}
+
+long long gcd(long long num1, long long num2) {
+  while (num2 != 0) {
+    long long temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
+  }
+  return num1;
+}
+
+long long findValidE(long long n, long long phi) {
+
+  if (gcd(3, phi) == 1 && gcd(3, n) == 1)
+    return 3;
+  if (gcd(65537, phi) == 1 && gcd(65537, n) == 1)
+    return 65537;
+
+  for (long long i = 2; i < phi; i++) {
+    if (gcd(i, n) == 1 && gcd(i, phi) == 1) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+void createKeys(long long p, long long q) {
+  long long n = p * q;
+  long long phi = (p - 1) * (q - 1);
+
+  long long e = findValidE(n, phi);
+  if (e == 0) {
+    printf("No valid e values\n");
+    return;
+  }
+
+  long long d = modInverse(e, phi);
+  if (d == -1) {
+    printf("No valid values for d\n");
+    return;
+  }
+
+  printf("The encryption key is: %lld, %lld\n", e, n);
+
+  printf("The decryption key is: %lld, %lld\n\n", d, n);
+}
+
+int main(int argc, char **argv) {
+  char mode;
+  printf("type e to encrypt, d to decrypt, c to create key, and g to generate "
+         "random key: ");
+  scanf(" %c", &mode);
+  getchar();
+
+  if (mode == 'g') {
+    srand(time(NULL));
+    int lowerLimit = 10000;
+    int upperLimit = 50000;
+    long long p = lowerLimit + rand() % (upperLimit - lowerLimit);
+    long long q = lowerLimit + rand() % (upperLimit - lowerLimit);
+
+    while (!isValidPrime(p, upperLimit)) {
+      p = lowerLimit + rand() % (upperLimit - lowerLimit);
+    }
+    while (!isValidPrime(q, upperLimit)) {
+      q = lowerLimit + rand() % (upperLimit - lowerLimit);
+    }
+
+    createKeys(p, q);
+  }
+
+  if (mode == 'c') {
+    long long p;
+    long long q;
+
+    bool checkPrimes = true;
+
+    printf("First prime number under 50000: ");
+    scanf("%lld", &p);
+
+    if (checkPrimes && !isValidPrime(p, 50000)) {
+      printf("Not a valid number\n");
+      return 0;
+    }
+
+    printf("Second prime number under 50000: ");
+    scanf("%lld", &q);
+
+    if (checkPrimes && !isValidPrime(q, 50000)) {
+      printf("Not a valid number\n");
+      return 0;
+    }
+
+    createKeys(p, q);
+  }
+  if (mode == 'e') {
+    long long key1, key2;
+    char message[MAX_MESSAGE_SIZE];
+    char ciphertext[MAX_MESSAGE_SIZE * FIXED_WIDTH] = {0};
+
+    printf("enter the encryption key in the form num1 num2: ");
+    scanf("%lld %lld", &key1, &key2);
+    getchar();
+
+    printf("enter message: ");
+    fgets(message, MAX_MESSAGE_SIZE, stdin);
+
+    for (int i = 0; i < strlen(message); i++) {
+      long long encrypted_char = modXP((long long)message[i], key1, key2);
+
+      char encrypted_str[FIXED_WIDTH + 1];
+      snprintf(encrypted_str, sizeof(encrypted_str), "%0*llx", FIXED_WIDTH,
+               encrypted_char);
+      strncat(ciphertext, encrypted_str,
+              sizeof(ciphertext) - strlen(ciphertext) - 1);
+    }
+
+    printf("ciphertext: %s\n", ciphertext);
+  }
+  if (mode == 'd') {
+    long long key1, key2;
+    char ciphertext[MAX_MESSAGE_SIZE * FIXED_WIDTH];
+    char message[MAX_MESSAGE_SIZE] = {0};
+
+    printf("enter the decryption key in the form num1 num2: ");
+    scanf("%lld %lld", &key1, &key2);
+    getchar();
+
+    printf("enter ciphertext: ");
+    fgets(ciphertext, MAX_MESSAGE_SIZE * FIXED_WIDTH, stdin);
+
+    int blocks = strlen(ciphertext) / FIXED_WIDTH;
+    for (int i = 0; i < blocks; i++) {
+      char block[FIXED_WIDTH + 1] = {0};
+      strncpy(block, &ciphertext[i * FIXED_WIDTH], FIXED_WIDTH);
+
+      long long encrypted_char = strtoll(block, NULL, 16);
+      long long decrypted_char = modXP(encrypted_char, key1, key2);
+      message[i] = (char)decrypted_char;
+    }
+
+    printf("message: %s\n", message);
+  }
+}
